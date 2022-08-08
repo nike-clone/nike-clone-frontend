1cddd43210480639af3df25dafe184085cade5c5
youss95 <usekim7555@gmail.com>
2022-08-08 23:21:13 +0900

    commit

--- src/pages/cart/CartPage.js
+++ src/pages/cart/CartPage.js
@@ -1,11 +1,10 @@
-import EmptyCart from 'components/Cart/EmptyCart';
 import styled from 'styled-components';
 import shoe1 from 'assets/images/shoe1.jpg';
 import shoe2 from 'assets/images/shoe2.jpg';
 import shoe3 from 'assets/images/shoe3.jpg';
 import shoe4 from 'assets/images/shoe4.jpg';
 import Cart from 'components/Cart/Cart';
-
+import EmptyCart from 'components/Cart/EmptyCart';
 const CartHeader = styled.h2`
   font-weight: 600;
   font-size: 30px;
