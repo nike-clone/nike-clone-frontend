c4956b8157b744fb7e79e32afc10cb28e03429dd
youss95 <usekim7555@gmail.com>
2022-09-18 23:26:04 +0900

    fix: side menu login/logout 조건 추가

--- src/components/Header/Side/SideMenu.js
+++ src/components/Header/Side/SideMenu.js
@@ -1,6 +1,9 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { logoutUser } from 'features/user/userSlice';
+import { v4 as uuid } from 'uuid';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
+import { modalStateChange } from 'features/modal/modalSlice';
 
 const Container = styled.div`
   padding: 80px 10px;
@@ -9,9 +12,45 @@ const SideMenuLink = styled.div`
   margin-bottom: 20px;
   font-size: 24px;
 `;
-const SideMenu = ({ showModal }) => {
+const SideMenu = ({ showModal, refetch }) => {
+  const user = JSON.parse(sessionStorage.getItem('user'));
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { isSucess } = useSelector((state) => state.user);
+  const logout = (e) => {
+    e.preventDefault();
+    sessionStorage.removeItem('user');
+    sessionStorage.removeItem('tokenId');
+    localStorage.setItem('NC_GUEST_ID', `user-${uuid()}`);
+    dispatch(logoutUser());
+    if (!isSucess) {
+      navigate('/');
+    }
+    refetch();
+  };
+  const modalOpenHandler = () => {
+    dispatch(modalStateChange());
+  };
   return (
     <Container>
+      <SideMenuLink>
+        {user ? (
+          <Link to="/" onClick={logout}>
+            로그아웃
+          </Link>
+        ) : (
+          <>
+            <Link to="/join" onClick={showModal}>
+              멤버 가입
+            </Link>
+            <br />
+            <br />
+            <Link to="/" onClick={modalOpenHandler}>
+              로그인
+            </Link>
+          </>
+        )}
+      </SideMenuLink>
       <SideMenuLink onClick={showModal}>
         <Link to="/category?gender=Men">Men</Link>
       </SideMenuLink>
